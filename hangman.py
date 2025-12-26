9fdb9d5f79cb1b895b0eca9343ab14b4c4ee14b6
aldairhoyos <aldairdejesushoyostorres@gmail.com>
2025-12-26 14:04:35 -0400

    Added the methods for showing a hint of the origin of the team (country) and also the method for showing the underscores of the game itself

--- hangman.py
+++ hangman.py
@@ -8,47 +8,62 @@ class Hangman:
         
         # The dictionary is defined locally inside __init__
         word_bank = {
-            "england": [
+            "England": [
                 "Arsenal", "Aston Villa", "Bournemouth", "Brentford", "Brighton", 
                 "Chelsea", "Crystal Palace", "Everton", "Fulham", "Ipswich Town", 
                 "Leicester City", "Liverpool", "Manchester City", "Manchester United", 
                 "Newcastle United", "Nottingham Forest", "Southampton", "Tottenham Hotspur", 
                 "West Ham United", "Wolverhampton Wanderers"
             ],
-            "spain": [
+            "Spain": [
                 "Alaves", "Athletic Bilbao", "Atletico Madrid", "Barcelona", "Celta Vigo", 
                 "Espanyol", "Getafe", "Girona", "Las Palmas", "Leganes", 
                 "Mallorca", "Osasuna", "Rayo Vallecano", "Real Betis", "Real Madrid", 
                 "Real Sociedad", "Sevilla", "Valencia", "Valladolid", "Villarreal"
             ],
-            "france": [
+            "France": [
                 "Angers", "Auxerre", "Brest", "Le Havre", "Lens", 
                 "Lille", "Lyon", "Marseille", "Monaco", "Montpellier", 
                 "Nantes", "Nice", "PSG", "Reims", "Rennes", 
                 "Saint-Etienne", "Strasbourg", "Toulouse"
             ],
-            "italy": [
+            "Italy": [
                 "AC Milan", "Atalanta", "Bologna", "Cagliari", "Como", 
                 "Empoli", "Fiorentina", "Genoa", "Inter Milan", "Juventus", 
                 "Lazio", "Lecce", "Monza", "Napoli", "Parma", 
                 "Roma", "Torino", "Udinese", "Venezia", "Verona"
             ],
-            "germany": [
+            "Germany": [
                 "Augsburg", "Bayer Leverkusen", "Bayern Munich", "Bochum", "Borussia Dortmund", 
                 "Borussia Monchengladbach", "Eintracht Frankfurt", "Freiburg", "Heidenheim", "Hoffenheim", 
                 "Holstein Kiel", "Mainz", "RB Leipzig", "St Pauli", "Stuttgart", 
                 "Union Berlin", "Werder Bremen", "Wolfsburg"
             ],
-            "portugal": [
+            "Portugal": [
                 "Arouca", "AVS", "Benfica", "Boavista", "Braga", 
                 "Casa Pia", "Estoril", "Estrela Amadora", "Famalicao", "Farense", 
                 "Gil Vicente", "Moreirense", "Nacional", "Porto", "Rio Ave", 
                 "Santa Clara", "Sporting CP", "Vitoria Guimaraes"
             ],
-            "netherlands": [
+            "Netherlands": [
                 "Ajax", "Almere City", "AZ Alkmaar", "Feyenoord", "Fortuna Sittard", 
                 "Go Ahead Eagles", "Groningen", "Heerenveen", "Heracles Almelo", "NAC Breda", 
                 "NEC Nijmegen", "PEC Zwolle", "PSV Eindhoven", "RKC Waalwijk", "Sparta Rotterdam", 
                 "Twente", "Utrecht", "Willem II"
             ]
         }
+
+        self.league = random.choice(list(word_bank.keys()))
+        self.team = random.choice(word_bank[self.league]).lower()
+
+    def show_hint(self):
+        print(f"\nThe team you're trying to guess is from {self.league}")
+
+    def word_masker(self):
+        for i in self.team:
+            if i == " ":
+                print(" ", end = " ")
+            elif i.lower() in self.guesses:
+                print(i, end = " ")
+            else:
+                print("_", end = " ")
