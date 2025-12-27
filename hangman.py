680d110b97a973429f7fee47a353a43c9cfb0b47
aldairhoyos <aldairdejesushoyostorres@gmail.com>
2025-12-27 09:04:15 -0400

    Added the main loop for playing the game. Some errors were corrected in some of the previously implemented functionalities

--- hangman.py
+++ hangman.py
@@ -60,10 +60,11 @@ class Hangman:
         print(f"The team you're trying to guess is from {self.league}!")
 
     def word_masker(self):
-        print("\nYou need to guesss this football team:\n")
+        print("You need to guesss this football team:\n")
         for i in self.team:
-            if i == " ":
-                print(" ", end = " ")
+            # Reveal if it's a space OR if it's not a letter (like a hyphen)
+            if i == " " or not i.isalpha():
+                print(i, end = " ")
             elif i.lower() in self.guesses:
                 print(i, end = " ")
             else:
@@ -75,7 +76,7 @@ class Hangman:
         self.show_hint()
         print(f"These are your chances left for guessing the team: {self.max_errors - self.count}")
         if len(self.guesses) == 0: 
-            print("You have not made any guesses yet")
+            pass
         else:
             print("The letters you have used are the following: {", end = "")
             letters = 0
@@ -92,34 +93,71 @@ class Hangman:
             elif len(letter) > 1:
                 print("\nYou have to give us a letter and not a word as you just did. Try again!")
             else:
-                if letter in {"1", "2", "3", "4", "5", "6", "7", "8", "9", "0"}:
-                    print("\nYou have introduced a number but it should be a letter of the alphabet. Try again!")
+                if letter.isalpha() != True :
+                    print("\nYou did not introduce a letter. Try again!")
                 elif letter in self.guesses:
                     print("\nYou have already selected that letter before! Try another one!")
                 else:
-                    print(f"\nYou have successfully selected the letter {letter}")
                     break
         return letter
-    
+
     def process_guess(self, letter):
         self.guesses.append(letter)
         if letter not in self.team:
-            self.count +=1
-            print("\nSorry, your guess was wrong!")
+            self.count += 1
+            print("\nSorry, your guess was wrong!\n")
         else:
-            print("\nGreat! You have guessed a letter of the word correctly!")
+            print("\nGreat! You have guessed a letter of the team!\n")
     
     def check_victory(self):
         flag = True
         for i in self.team:
-            if i not in self.guesses:
+            # Skip spaces AND non-letters (like hyphens)
+            if i == " " or not i.isalpha():
+                pass
+            elif i not in self.guesses:
                 flag = False
                 break
         return flag
     
     def guess_word(self):
-        word = input("So... You want to guess the word, huh! Go ahead: ")
+        word = input("So... You want to guess the word, huh! Go ahead: ").lower()
         return True if word == self.team else False
 
     def game_over(self):
         return True if self.count >= self.max_errors else False
+    
+    def play_game(self):
+        print("Welcome to Football Hangman!")
+        while True:
+            self.game_status()
+            try:
+                print("\nSelect one of the following options:")
+                print("\n1) Guess the word right away")
+                print("\n2) Guess a letter")
+                option = int(input("\nYour choice: "))
+                if option not in {1, 2}:
+                    print("\nThere are only two options, you need to pick one of them! Try again!")
+                else:
+                    if option == 1:
+                        risky_guess = self.guess_word()
+                        if risky_guess == True:
+                            print("You guessed the team correctly! You are THE GOAT!!!!")
+                        else:
+                            print("There's a high price for taking such a big leap! You did not guess the team correctly.\nYou lost the game, my friend! See you next time!")
+                        break
+                    else:
+                        letter = self.user_input()
+                        self.process_guess(letter)
+            except ValueError:
+                print("\nYou need to introduce a numeric value in order to proceed... Try again!")
+            if self.check_victory():
+                print(f"\nYou guessed the team! It was {self.team}! Congratulations, you just won!")
+                break
+            if self.game_over():
+                print(f"\nYou ran out of guesses! You just lost the game, my friend! The team was {self.team}")
+                break
+
+if __name__ == "__main__":
+    player = Hangman(3)
+    player.play_game()
